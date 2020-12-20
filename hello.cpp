71832fe9ac12d88543e1b2907ce0a868994096de
ilariaBertoletti <32894358+ilariaBertoletti@users.noreply.github.com>
2020-12-20 23:51:37 +0100

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,5 @@ using namespace std;
 
 int main(){
 
-  cout << "Proviamo ancora git";
-  cout << "Io pero' aggiungo altre cose";
-  cout << "No non voglio aggiungere altre cose";
+  cout << "Proviamo git";
 }
